91866a20ba1897b18d8671a71fd3d94624cc84af
abraham17ramirez <144249908+abraham17ramirez@users.noreply.github.com>
2023-09-08 08:08:09 -0600

    subida del proyecto

---
+++ README.md
@@ -0,0 +1,6 @@
+# demo_scv
+
+proyecto_sistemacontrol_version
+
+IDE DE androi con flutter 
+REBECA ORTIZ 2023 
---
+++ android/app/src/main/java/com/rnoc/demo_scv/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rnoc.demo_scv;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
